bb45c5f61d806bd4ee814fbc139cf97977266232
Steve <nguyen.dn.2014@gmail.com>
2023-11-06 14:43:31 +0700

    First commit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a1897a581db0d480eb795c7271435590-838839899.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
